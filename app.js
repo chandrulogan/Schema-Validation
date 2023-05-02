12599d2f1e74d1aa4070d899f9569faf25b995c6
Chandru loganathan <78639242+chandrulogan@users.noreply.github.com>
2023-05-02 14:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,5 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log("Server is Running");
+})
 module.exports = app;
